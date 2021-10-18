a7e59cd312d425636ea6774a29b0da49a07670c1
Estian Eek <eek.estian@yahoo.com>
2021-10-18 23:45:23 +0200

    Homepage updated

--- WebApp/JS/homepage.js
+++ WebApp/JS/homepage.js
@@ -3,4 +3,29 @@ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
 
-let pictures = []
+let picturesTitle = ["Picture1", "Picture2", "Picture3", "Picture4", "Picture5",
+    "Picture6", "Picture7", "Picture8"
+]
+
+const popoversImg = Array.from(document.getElementsByClassName("popover-image"));
+const popovers = Array.from(document.getElementsByClassName("pop-over"));
+
+/*popoversImg.forEach(popoverImg => {
+    const number = popoverImg.dataset['number'];
+    popoverImg.['title'] = popovers[number];
+});
+*/
+
+setImageNames = () => {
+    popovers.forEach(popover => {
+        const number = popover.dataset['number'];
+        //popover.setAttribute("title", picturesTitle[number - 1]);
+        popover.setAttribute("title", picturesTitle[number - 1]);
+    });
+    
+};
+
+setImageNames();
+
+
+console.log(popovers);
