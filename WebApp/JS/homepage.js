2a67d0acdf264168928f7611975bd8c0d3d5740e
Estian Eek <eek.estian@yahoo.com>
2021-11-12 15:07:34 +0200

    accordions added

--- WebApp/JS/homepage.js
+++ WebApp/JS/homepage.js
@@ -1,34 +1,14 @@
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-});
 
-let picturesTitle = ["Picture1", "Picture2", "Picture3", "Picture4", "Picture5",
-    "Picture6", "Picture7", "Picture8"
-];
-
-const popoversImg = Array.from(document.getElementsByClassName("popover-image"));
-const popovers = Array.from(document.getElementsByClassName("pop-over"));
-const popoverText = document.getElementById("pop5");
-
-/*popoversImg.forEach(popoverImg => {
-    const number = popoverImg.dataset['number'];
-    popoverImg.['title'] = popovers[number];
-});
-*/
-
-setImageNames = () => {
-    popovers.forEach(popover => {
-        const number = popover.dataset['number'];
-        //popover.setAttribute("title", picturesTitle[number - 1]);
-        popover.setAttribute("title", picturesTitle[number - 1]);
-        popoverText.setAttribute(popoverText.dataset["bs-content"], "pic5");
-        document.getElementsByClassName("pop-over")[0].setAttribute("title", "raeguh");
-    });
+createAccordionImages = () => {
+    const img = document.createElement("img");
+    img.src = "/PhotoSharingWebApp/WebApp/Images/DSCF2588.JPG" 
+    img.classList.add("images-btn")
     
-};
+    document.getElementById("accordion-image").appendChild(img);
+}
 
-setImageNames();
+
+createAccordionImages();
 
 
-console.log(popovers);
+
